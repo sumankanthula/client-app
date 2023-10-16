4d54411ee78f95df7ec5e9829239f5513ecc36cc
suman.kanthula@live.com <suman.kanthula@live.com>
2023-10-16 02:26:17 -0500

    Test

--- src/main.js
+++ src/main.js
@@ -1,26 +1,15 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
-//import { axios  } from 'vue-axios';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-//Vue.use(axios);
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
-
-// import Vue from 'vue'
-// import App from './App.vue'
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
